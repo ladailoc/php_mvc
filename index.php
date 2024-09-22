f5c8377486c07fa4adc1fac752438d615cc33884
ladailoc <ladailoc9a3@gmail.com>
2024-09-23 01:38:50 +0700

    dailoc: config

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello World!";
+session_start();
+require_once 'bootstrap.php';
+$app = new App();
+
+
+
+
+if (!empty($_SERVER['PATH_INFO'])){
+    $url = $_SERVER['PATH_INFO'];
+}
+else {
+    $url = '/';
+}
+echo $url;
